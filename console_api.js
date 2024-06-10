ed366640f60235b014f0bc08d3b0c321f35d3538
Shoichiro Sakaigawa <sakaigawa@pumpkinheads.jp>
2024-06-10 16:15:44 +0000

    add axios debug log

--- console_api.js
+++ console_api.js
@@ -8,27 +8,43 @@ class ConsoleRESTAPI {
         this.CLIENT_ID = client_id;
         this.CLIENT_SECRET = client_secret;
         this.GCS_OKTA_DOMAIN = gcs_okta_domain;
-        this.AUTHORIZATION_CODE = base64.encode(`${client_id}:${client_secret}`);
+        this.AUTHORIZATION_CODE = base64.encode(`${this.CLIENT_ID}:${this.CLIENT_SECRET}`);
+
+        // Axiosインスタンスの作成
+        this.axiosInstance = axios.create({
+            baseURL: this.BASE_URL
+        });
+
+        // リクエストインターセプターの設定
+        this.axiosInstance.interceptors.request.use(request => {
+            console.log('Starting Request: ', request);
+            return request;
+        });
+
+        // レスポンスインターセプターの設定
+        this.axiosInstance.interceptors.response.use(response => {
+            console.log('Response: ', response);
+            return response;
+        });
     }
 
     async getToken() {
-        if (!this.GCS_OKTA_DOMAIN) {
-            throw new Error(`The "GCS_OKTA_DOMAIN" argument is missing. Received: ${this.GCS_OKTA_DOMAIN}`);
-        }
         const headers = {
-            "accept": "application/json",
-            "authorization": `Basic ${this.AUTHORIZATION_CODE}`,
-            "cache-control": "no-cache",
-            "content-type": "application/x-www-form-urlencoded",
+            'accept': 'application/json',
+            'authorization': `Basic ${this.AUTHORIZATION_CODE}`,
+            'cache-control': 'no-cache',
+            'content-type': 'application/x-www-form-urlencoded'
         };
 
         const data = qs.stringify({
-            "grant_type": "client_credentials",
-            "scope": "system",
+            'grant_type': 'client_credentials',
+            'scope': 'system'
         });
 
         try {
-            const response = await axios.post(this.GCS_OKTA_DOMAIN, data, { headers });
+            const response = await this.axiosInstance.post(this.GCS_OKTA_DOMAIN, data, { headers });
+            console.log("get token:");
+            console.dir(response);
             return response.data.access_token;
         } catch (error) {
             throw new Error(`Error fetching token: ${error.message}`);
@@ -37,59 +53,69 @@ class ConsoleRESTAPI {
 
     async getHeaders(payload) {
         const token = await this.getToken();
-        const headers = { "Accept": "application/json", "Authorization": `Bearer ${token}` };
+        const headers = { 'Accept': 'application/json', "Authorization": `Bearer ${token}` };
         if (Object.keys(payload).length !== 0) {
-            headers["Content-Type"] = "application/json";
+            headers['Content-Type'] = 'application/json';
         }
         return headers;
     }
 
     async request(url, method, options = {}) {
-        let params = {};
-        let payload = {};
-        url = this.BASE_URL + url;
+        let { params = {}, payload = {}, files = {} } = options;
+        url = `${this.BASE_URL}${url}`;
 
-        for (let key in options) {
-            if (options[key] !== null && options[key] !== undefined) {
+        for (const [key, val] of Object.entries(options)) {
+            if (val !== null && val !== undefined) {
                 if (key === 'payload') {
-                    payload = options[key];
+                    payload = JSON.stringify(val);
+                } else if (key === 'files') {
+                    files = val;
                 } else {
                     if (url.includes(`{${key}}`)) {
-                        url = url.replace(`{${key}}`, options[key]);
+                        url = url.replace(`{${key}}`, val);
                     } else {
-                        params[key] = options[key].toString();
+                        params[key] = String(val);
                     }
                 }
             }
         }
 
         const headers = await this.getHeaders(payload);
+        console.log("--------------------");
+        console.dir(method);
+        console.log("url" + url);
+        console.log("headers");
+        console.log(headers);
+        console.log("data:");
+        console.dir(payload);
+        console.log("param:");
+        console.dir(params);
 
         try {
-            const response = await axios({
+            const response = await this.axiosInstance({
                 method,
                 url,
                 headers,
                 params,
                 data: payload,
+                files
             });
+
+            console.log("request:");
+            console.dir(response);
+
             return response.data;
         } catch (error) {
-            throw new Error(`Request error: ${error.message}`);
+            throw new Error(`Error in request: ${error.message}`);
         }
     }
 
-    async getDevices() {
-        return await this.request("/devices", "GET", {});
-    }
-
-    async getImageDirectories(device_id) {
-        return await this.request("/devices/images/directories", "GET", { device_id });
-    }
-
-    async getInferenceResults(device_id, number_of_results, filter, raw, time) {
-        return await this.request(`/devices/${device_id}/inferenceresults`, "GET", {
-            number_of_results, filter, raw, time
+    async getDevices(connectionState = null, device_name = null, device_id = null, device_group_id = null) {
+        return this.request('/devices', 'GET', {
+            connectionState,
+            device_name,
+            device_id,
+            device_group_id
         });
     }
 }
--- getDevices.js
+++ getDevices.js
@@ -5,7 +5,6 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         const node = this;
         const configNode = RED.nodes.getNode(config.config);
-        node.log(config.config);
 
         if (configNode) {
             node.log(`ConfigNode: baseURL=${configNode.baseURL}, client_id=${configNode.client_id}, client_secret=${configNode.client_secret}, gcs_okta_domain=${configNode.gcs_okta_domain}`);
@@ -14,18 +13,23 @@ module.exports = function (RED) {
         }
 
         node.on('input', async function (msg) {
-            
             if (!configNode || !configNode.baseURL || !configNode.client_id || !configNode.client_secret || !configNode.gcs_okta_domain) {
                 node.error("Missing configuration parameters", msg);
                 return;
             }
+
             const consoleAPI = new ConsoleRESTAPI(configNode.baseURL, configNode.client_id, configNode.client_secret, configNode.gcs_okta_domain);
             try {
                 const response = await consoleAPI.getDevices();
+                console.log("---------------");
+                console.log("!!!response!!!");
+                console.dir(response);
+                console.log("---------------");
+
                 msg.payload = response;
                 node.send(msg);
             } catch (error) {
-                node.error(error.message, msg);
+                node.error(`Error fetching token: ${error.message}`, msg);
             }
         });
     }
